87ce6f569fedef02724166b67fe571a897457740
Dan Mishin <mishin.danil@mail.ru>
2020-09-05 00:21:10 +0500

    Main

---
+++ sort.js
@@ -0,0 +1,22 @@
+var fs = require('fs'),
+  spawn = require('child_process').spawn,
+  sort = spawn('sort', ['in.txt']);
+  
+var writer = fs.createWriteStream('out.txt');
+
+var start = Date.now();
+console.log('Let\'s try to sort!')
+
+sort.stdout.on('data', function (data) {
+  writer.write(data);
+});
+
+sort.on('exit', function () {
+  writer.end();
+  var closetime = Date.now();
+  console.log('Job done with time:', (closetime - start) / 1000, ' secs');
+});
+
+sort.stderr.on('data', function (data) {
+  if(data) console.log('Err: ' + data);
+});
