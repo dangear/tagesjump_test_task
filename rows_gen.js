142b1ddf73d3b595a6ca95a3879055b4b1aad127
Dan Mishin <mishin.danil@mail.ru>
2020-09-07 01:22:36 +0500

    Minor

--- rows_gen.js
+++ rows_gen.js
@@ -1,19 +1,20 @@
-var fs = require('fs');
+var fs = require("fs");
 var { nanoid } = require("nanoid/non-secure");
-var size=10; //Default file size in Mb
 
-var writeFile = fs.openSync('in.txt', 'w'); //Open file to write
+var defaultSize = 10; //Default file size in Mb
 
-if(process.argv[2]) size=process.argv[2] //Get argument if exist
+var writeFile = fs.openSync("in.txt", "w"); //Open file to write
 
-let limit = 64 * 1024 * size; //Set limit for output file
+if(process.argv[2]) defaultSize = process.argv[2] //Get argument if exist
 
-console.log('Generating a test file with size:',size+'Mb')
+let sizeLimit = 64 * 1024 * defaultSize; //Set sizeLimit for output file
 
-for(let i=0; i<limit; i++) {
-  fs.writeSync(writeFile, nanoid(15) + '\n', null, 'utf8'); //Generate and write string to the file
+console.log(`Generating a test file with size: ${defaultSize} Mb`)
+
+for (let i = 0; i < sizeLimit; i++) {
+  fs.writeSync(writeFile, nanoid(15) + "\n", null, "utf8"); //Generate and write string to the file
 }
 
-console.log('Test file is generated!');
+console.log("Test file is generated!");
 
-fs.closeSync(writeFile); //Close file 
+fs.closeSync(writeFile); //Close file
--- sort.js
+++ sort.js
@@ -1,23 +1,23 @@
-var fs = require('fs'),
-  spawn = require('child_process').spawn,
-  sort = spawn('sort', ['in.txt']);  //use system sort util
+var fs = require("fs");
+var spawn = require("child_process").spawn;
+var sort = spawn("sort", ["in.txt"]);  //use system sort util
 
-var start = Date.now(); //Start timer for monit if need it
+var startTimer = Date.now(); //Start timer for monit if need it
 
-var writer = fs.createWriteStream('out.txt'); //create output file
+var writer = fs.createWriteStream("out.txt"); //create output file
 
-console.log('Let\'s try to sort!');
+console.log("Let's try to sort!");
 
-sort.stdout.on('data', function (data) { //when get data from sort util
-  writer.write(data); //write data to output file
+sort.stdout.on("data", function (sortedData) { //when get data from sort util
+  writer.write(sortedData); //write data to output file
 });
 
-sort.on('exit', function () { //when sort util job is done
+sort.on("exit", function () { //when sort util job is done
   writer.end(); //close output file
-  var closetime = Date.now(); //Stop timer for monit if need it
-  console.log('Job done with time:', (closetime - start) / 1000, ' secs'); //Show job time
+  var stopTimer = (Date.now() - startTimer) / 1000; //Stop timer for monit if need it
+  console.log(`Job done with time: ${stopTimer} secs`); //Show job time
 });
 
-sort.stderr.on('data', function (data) { //if sort util return error
-  if(data) console.log('Err: ' + data); //console it
+sort.stderr.on("data", function (err) { //if sort util return error
+  if(err) console.log("Err: " + err); //console it
 });
