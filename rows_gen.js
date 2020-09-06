49a0b2336a16a749850fe026ccc521037263769a
Dan Mishin <mishin.danil@mail.ru>
2020-09-06 23:17:26 +0500

    Add comments

--- rows_gen.js
+++ rows_gen.js
@@ -1,19 +1,19 @@
 var fs = require('fs');
 var { nanoid } = require("nanoid/non-secure");
-var size=10;
+var size=10; //Default file size in Mb
 
-var writeFile = fs.openSync('in.txt', 'w');
+var writeFile = fs.openSync('in.txt', 'w'); //Open file to write
 
-if(process.argv[2]) size=process.argv[2]
+if(process.argv[2]) size=process.argv[2] //Get argument if exist
 
-let limit = 64 * 1024 * size;
+let limit = 64 * 1024 * size; //Set limit for output file
 
 console.log('Generating a test file with size:',size+'Mb')
 
 for(let i=0; i<limit; i++) {
-  fs.writeSync(writeFile, nanoid(15) + '\n', null, 'utf8');
+  fs.writeSync(writeFile, nanoid(15) + '\n', null, 'utf8'); //Generate and write string to the file
 }
 
 console.log('Test file is generated!');
 
-fs.closeSync(writeFile);
+fs.closeSync(writeFile); //Close file 
--- sort.js
+++ sort.js
@@ -1,23 +1,23 @@
 var fs = require('fs'),
   spawn = require('child_process').spawn,
-  sort = spawn('sort', ['in.txt']);
+  sort = spawn('sort', ['in.txt']);  //use system sort util
 
-var start = Date.now();
+var start = Date.now(); //Start timer for monit if need it
 
-var writer = fs.createWriteStream('out.txt');
+var writer = fs.createWriteStream('out.txt'); //create output file
 
 console.log('Let\'s try to sort!');
 
-sort.stdout.on('data', function (data) {
-  writer.write(data);
+sort.stdout.on('data', function (data) { //when get data from sort util
+  writer.write(data); //write data to output file
 });
 
-sort.on('exit', function () {
-  writer.end();
-  var closetime = Date.now();
-  console.log('Job done with time:', (closetime - start) / 1000, ' secs');
+sort.on('exit', function () { //when sort util job is done
+  writer.end(); //close output file
+  var closetime = Date.now(); //Stop timer for monit if need it
+  console.log('Job done with time:', (closetime - start) / 1000, ' secs'); //Show job time
 });
 
-sort.stderr.on('data', function (data) {
-  if(data) console.log('Err: ' + data);
+sort.stderr.on('data', function (data) { //if sort util return error
+  if(data) console.log('Err: ' + data); //console it
 });
